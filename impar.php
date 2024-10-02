727a92f3f75a6d5ec1b988bc26d309df2e6215e8
LewiFZZ <levijosuecandeiasdefigueiredo.guadalupe@alumnado.fundacionloyola.net>
2024-10-02 15:44:06 +0100

    Se han implementado las paginas de par e impar

---
+++ impar.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Introducir Número</title>
+        <link href="css/impar.css" rel="stylesheet" />
+    </head>
+    <body>
+        <main>
+            <h1>Resultado</h1>
+            <table>
+                <tr>
+                    <?php
+                        //Se guarda los elementos del array asociativo get en variables individuales
+                        $numero = $_GET['numero'];
+                        $cuadrado = $_GET['cuadrado'];
+
+                        echo "<td>".$numero."</td>";
+                        echo "<td>".$cuadrado."</td>";                       
+                    ?>
+                    <td>Impar</td>
+                </tr>
+            </table>
+        </main>
+    </body>
+</html>
---
+++ par.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Introducir Número</title>
+        <link href="css/par.css" rel="stylesheet" />
+    </head>
+    <body>
+        <main>
+            <h1>Resultado</h1>
+            <table>
+                <tr>
+                    <?php
+                        //Se guarda los elementos del array asociativo get en variables individuales
+                        $numero = $_GET['numero'];
+                        $cuadrado = $_GET['cuadrado'];
+
+                        echo "<td>".$numero."</td>";
+                        echo "<td>".$cuadrado."</td>";                       
+                    ?>
+                    <td>Par</td>
+                </tr>
+            </table>
+        </main>
+    </body>
+</html>
